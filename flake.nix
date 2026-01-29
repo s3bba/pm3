242b2f4a8098069268589bb6a258776150f40eb5
frectonz <fraol0912@gmail.com>
2026-01-30 00:46:36 +0300

    chore: big bang

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      forAllSystems =
+        fn:
+        let
+          systems = [
+            "x86_64-linux"
+            "aarch64-darwin"
+          ];
+          overlays = [ (import rust-overlay) ];
+        in
+        nixpkgs.lib.genAttrs systems (
+          system:
+          fn (
+            import nixpkgs {
+              inherit system overlays;
+            }
+          )
+        );
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.fzf
+            pkgs.pnpm
+            pkgs.just
+            pkgs.bacon
+            pkgs.nodejs
+            pkgs.openssl
+            pkgs.cargo-dist
+            pkgs.pkg-config
+            pkgs.rust-analyzer
+            pkgs.rust-bin.stable.latest.default
+          ];
+        };
+      });
+
+      formatter = forAllSystems (
+        pkgs:
+        pkgs.treefmt.withConfig {
+          runtimeInputs = [ pkgs.nixfmt-rfc-style ];
+          settings = {
+            on-unmatched = "info";
+            formatter.nixfmt = {
+              command = "nixfmt";
+              includes = [ "*.nix" ];
+            };
+          };
+        }
+      );
+    };
+}
